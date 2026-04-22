b65873e59c646d58eb60a66f87742fcd0fbbc862
Zhenia <zhenia.saharow@yandex.ru>
2026-04-22 19:41:55 +0300

    Modify settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
